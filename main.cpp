34e6b37301b514d46b8f637c47790c248e268020
valnhe <vpazcastle@gmail.com>
2024-11-12 19:18:24 -0300

    move initialsolution

--- main.cpp
+++ main.cpp
@@ -20,29 +20,29 @@ int main(int argc, char* argv[]) {
     std::vector<Vertex> hoteles;
     std::vector<Vertex> pois;
 
-    int restart = 10;
+    int restart = 20;
     int MAX_ITER = 100;
 
     //Ler achivo de instancia
     instances(nombreArchivo, N, H, D, Tmax, Td, hoteles, pois);
-    
-    //Generar solución inicial
-    Solucion solucionInicial = generarSolucionInicial(hoteles, pois, Td, D);
-    std::cout << " >>>> Solución Inicial:\n" << std::endl;
-    printearSoluciones(solucionInicial);
 
     //Ejecutar Hill Climbing + MM
-    Solucion mejorSolucion = hillClimbing (restart, MAX_ITER, solucionInicial, hoteles, pois, Td, D);
+    std::vector<Solucion> mejoresSoluciones = hillClimbing (restart, MAX_ITER, hoteles, pois, Td, D);
 
     //Comparación solución inicial con solución local
-    std::cout << "\n\n - Iteraciones Terminadas - \n";
-    std::cout << " >>>> Solución Inicial:\n" << std::endl;
-    printearSoluciones(solucionInicial);
+    std::cout << "\n - Iteraciones Terminadas - \n";
 
-    std::cout << "\n >>>> Solución Local Encontrada:\n" << std::endl;
-    printearSoluciones(mejorSolucion);
-    std::cout << "\n\n";
-    std::cout << " ";
+    int i = 1;
+    Solucion mejorSol;
+    mejorSol.puntajeTotal = 0.0;
+
+    for (const auto& solucion : mejoresSoluciones) {
+        if (solucion.puntajeTotal > mejorSol.puntajeTotal) {
+            mejorSol = solucion;
+        }
+    }
+
+    printearSoluciones(mejorSol);
 
     return 0;
 }
